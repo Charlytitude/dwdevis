95bd08140058117505c9994ba4dd850aef5fd7f3
Charlytitude <Charlytitude@users.noreply.github.com>
2016-02-06 11:56:24 +0100

    Create step-merci.php

---
+++ front/step-merci.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: mverrecchia
+ * Date: 01/07/13
+ * Time: 17:15
+ */
+?>
+
+<img src="<?php echo DWDevisApplication::getImgUrl('head_page_merci.jpg') ?>" alt="" height="100%" width="auto" style="margin-bottom:30px;"/>
