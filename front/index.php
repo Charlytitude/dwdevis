6cbf07bf49124a445e8310e0f4b637d75556d573
Charlytitude <Charlytitude@users.noreply.github.com>
2016-02-06 11:57:51 +0100

    Update index.php

--- front/index.php
+++ front/index.php
@@ -1,78 +1,144 @@
 <?php
 /**
- * User: mverrecchia
- * Date: 01/07/13
- * Time: 16:15
+ * User: maff
+ * Date: 29/06/13
  */
 
-/*if (isset($_POST['devis'])){
+/**
+ * Define $action
+ */
+
+if(isset($_POST['action'])){
+    $action = $_POST['action'];
+}else{
+    $action = '';
+}
+
+if (isset($_POST['devis'])){
     //check validity
     if (isset($_POST['key']) && $_POST['key'] == DevisUtils::saltDevis($_POST['devis'])){
         $devis = new Devis($_POST['devis']);
-   
-	}else{
-	
-        // @todo rediriger les hackers 
-        header('Location:/');
-   }
+        if (isset($_POST['dommage'])){
+            if (isset($_POST['key2']) && $_POST['key2'] == DevisUtils::saltDommage($_POST['dommage'])){
+                $dommage = new Dommage($_POST['dommage']);
+                $dommage->setDevis($devis);
+                $piece = $dommage->getPiece();
+            }else{
+                /* @todo rediriger les hackers */
+                //header('Location:/');
+            }
+        }else{
+            $dommage = new Dommage();
+        }
+        if (isset($_POST['piece'])){
+            /* var $piece Piece */
+            $piece = DWDevisApplication::$pieces[$_POST['piece']];
+        }
+
+    }else{
+        /* @todo rediriger les hackers */
+        //header('Location:/');
+    }
 }else{
-    //todo faire qq chose
-}*/
+    $devis = new Devis();
+    $dommage = new Dommage();
+}
+
 
 ?>
 
-<h6 class="step-title">R&eacute;capitulatif : Merci de v&eacute;rifier les &eacute;l&eacute;ments ci-dessous puis valider votre demande</h6>
-<div class="recap_gauche"> 
-    <h4>Vous</h4>
-        <ul>
-            <li><span><?php echo $devis->getUtilisateur() ?></span></li>
-            <li><span><?php echo $devis->getUtilisateur()->getAdresse() ?></span></li>
-            <?php if($devis->getUtilisateur()->getComplementAdresse()): ?>
-                <li><span><?php echo $devis->getUtilisateur()->getComplementAdresse() ?></span></li>
-            <?php endif; ?>
-            <li><span><?php echo $devis->getUtilisateur()->getCp() . ' ' . $devis->getUtilisateur()->getVille() ?></span></li>
-        </ul>
-    <h4>Votre v&eacute;hicule</h4>
-        <ul>
-            <li><?php echo $devis->getVehicule() ?></li>
-            <li><?php echo $devis->getVehicule()->getKms() ?> kms</li>
-        </ul>
-</div>
 
-<div class="recap_droite">
-        <h4>Dommages d&eacute;clar&eacute;s</h4>
-        <ul>
-            <?php foreach ($devis->getDommages() as $dommage): ?>
-                <li>
-                    <h6>Pi&egrave;ce: <?php echo $dommage->getPiece() ?></h6>
-                    <ul>
-                        <li>Taille : <?php echo EnumTailleDommage::$libs[$dommage->getTaille()] ?></li>
-                        <li>Type : <?php echo EnumTypeDommage::$libs[$dommage->getType()] ?></li>
-                        <li>Peinture abîmée : <?php echo EnumPeintureDommage::$libs[$dommage->getPeinture()] ?></li>
-                        <li>Commentaire : <?php echo htmlentities($dommage->getCommentaire()) ?></li>
-                        <li>
-                            Photos :
-                            <?php $i=1; foreach($dommage->getPhotos() as $photo) : ?>
-                                <span><a target="blank" href="<?php echo DevisUtils::getPhotoUrl($photo->getName()) ?>">photo <?php echo $i++ ?></a></span>
-                            <?php endforeach; ?>
-                        </li>
-                    </ul>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </li>
-</ul>
-</div>
 
-<div class="recap_validation">
+<?php
+switch($action){
 
-    <form class="fm-form form-devis" id="devis_dommage" method="post" action="">
-        <input type="hidden" name="devis" value="<?php echo $devis->getId(); ?>" />
-        <input type="hidden" name="key" value="<?php echo DevisUtils::saltDevis($devis->getId()); ?>" />
-        <input type="hidden" name="action" value="<?php echo Action::VALIDATE ?>" />
+    case Action::SAVE_IDENTIFICATION:
+        $return = hydrateDevisIdentification($_POST);
 
-        <input type="submit" name="fm_form_submit" id="fm_form_submit" class="submit" value="Valider">
+        $errors = $return['errors'];
+        $devis = $return['devis'];
 
-    </form>
+        if(count($errors) > 0){
+            include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-identification.php');
+        }else{
+            $devis->save();
+            include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-elements.php');
+        }
+        break;
+
+    case Action::CHOOSE_PIECE:
+        /* TODO : contrôler validité pièce */
+
+        if($devis->hasDommageForPiece($piece)){ //on est dans le cas d'une piece existante
+            $dommage = $devis->getDommageForPiece($piece);
+        }else{ // il s'agit d'un nouveau dommage
+            $dommage->setPiece($piece);
+        }
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-elements.php');
+        break;
+
+    /* CREATION DOMMAGE */
+    case Action::ADD_DOMMAGE:
+
+        /* TODO : contrôler validité */
+        $return = hydrateDommage($_POST, $_FILES);
+        $dommage = $return['dommage'];
+        $dommage->setDevis($devis);
+        $dommage->setPiece($piece);
+
+        $dommage->save();
+        $devis->addDommage($dommage);
+
+        /* on reset le dommage */
+        $dommage = new Dommage();
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-elements.php');
+        break;
+
+    case Action::UPDATE_DOMMAGE:
+
+        $return = hydrateDommage($_POST, $_FILES, $dommage);
+        $dommage = $return['dommage'];
+        $dommage->save();
+
+        $dommage = new Dommage();
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-elements.php');
+        break;
+
+    case Action::DELETE_PHOTO:
+        /* TODO controles */
+
+        $photo = new Photo($_POST['photo']);
+        $dommage->removePhoto($photo);
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-elements.php');
+        break;
+
+    case Action::DELETE_DOMMAGE:
+
+        $devis->removeDommage($dommage);
+        $dommage = new Dommage();
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-elements.php');
+        break;
+
+    case Action::SHOW_RECAPITULATIF:
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-recap.php');
+        break;
+
+    case Action::VALIDATE:
+        /* TODO envoyer email */
+        $devis->setState(EnumDevisState::FINALISED);
+        $devis->save();
+        DevisHelper::sendConfirmationEmail($devis);
+        DevisHelper::sendInternalEmail($devis);
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-merci.php');
+        break;
+
+    default:
+        $errors = array();
+        include (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'step-identification.php');
+        break;
+
+}
+
+?>
 
 </div>
